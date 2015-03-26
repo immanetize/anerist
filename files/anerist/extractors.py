146c3fc0f6de9f5d504a4ac99ad4047f74077c86
Pete Travis <immanetize@fedoraproject.org>
2015-03-26 00:07:08 -0600

    make xml metadata extraction more functional

--- files/anerist/extractors.py
+++ files/anerist/extractors.py
@@ -6,7 +6,7 @@
 import os
 import ConfigParser
 from bs4 import BeautifulSoup
-
+import yaml
 
 configfile = 'publican.cfg'
 
@@ -25,38 +25,51 @@ class FakeSecHead(object):
         else: 
             return self.fp.readline()
 
-pcfg = ConfigParser.SafeConfigParser()
-pcfg.readfp(FakeSecHead(open(configfile)))
-book_type = pcfg.get('pants', 'type')
-info_file = "en-US/%s_Info.xml" % book_type
-info = open(info_file)
+def _read_publican_config(configfile, lang='en-US'):
+    pcfg = ConfigParser.SafeConfigParser()
+    pcfg.readfp(FakeSecHead(open(configfile)))
+    book_type = pcfg.get('pants', 'type')
+    info_file = "%s/%s_Info.xml" % (lang, book_type)
+    info = open(info_file)
 
-docsoup = BeautifulSoup(info)
+    docsoup = BeautifulSoup(info)
 
-headline = docsoup.title.string
-dope = docsoup.abstract.para.string
-kicker = docsoup.subtitle.string
+    title = docsoup.title.string
+    stub = docsoup.subtitle.string
+    abstract = docsoup.abstract.para.string
 
-for root, dirs, files in os.walk('en-US'):
-    for name in files:
-        if name.endswith('ent'):
-            entity_file = os.path.join(root, name)
-entity_list = []
-entity_list.extend(open(entity_file))
+    for root, dirs, files in os.walk(lang):
+        for name in files:
+            if name.endswith('ent'):
+                entity_file = os.path.join(root, name)
 
-for line in entity_list:
-    entlist = line.split()
-    if len(entlist):
-        ent[entlist[1]] = " ".join(entlist[2:].strip('"')
+    entity_list = []
+    ent = {}
+    entity_list.extend(open(entity_file))
+
+    for line in entity_list:
+        entlist = line.split()
+        if len(entlist):
+            ent[entlist[1]] = " ".join(entlist[2:].strip('"')
                 
-for item in ent:
-    ent[item] = re.sub('>$', '', ent[item])
+    for item in ent:
+        ent[item] = re.sub('>$', '', ent[item])
+    for item in ent:
+        for value in ent:
+            ent[value] = re.sub('&%s;' % item, ent[item], ent[value])
 
-for item in ent:
-    for value in ent:
-        ent[value] = re.sub('&%s;' % item, ent[item], ent[value])
+    for item in ent:
+        title = re.sub('&%s;' % item, ent[item], title)
+        stub = re.sub('&%s;' % item, ent[item], stub)
+        abstract = re.sub('&%s;' % item, ent[item], abstract)
 
-for item in ent:
-    dope = re.sub('&%s;' % item, ent[item], dope)
+    return title, stub, abstract        
+y = open('metadata.yml')
+meta = yaml.load(y)
+y.close()
 
+# do stuff to meta
+y = open('metadata.yml', 'w')
+yaml.dump(meta, y, default_flow_style=False)
+y.close()
 
