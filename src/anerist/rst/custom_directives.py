6f96c6c787b4e251d3b6de211e71bf9b7986ca92
Pete Travis <immanetize@fedoraproject.org>
2015-09-09 09:42:22 -0400

    moah metadata directives

--- src/anerist/rst/custom_directives.py
+++ src/anerist/rst/custom_directives.py
@@ -9,7 +9,7 @@ class slug(Special, Invisible, FixedTextElement):
 class Slug(rst.Directive):
     required_arguments = 1
     optional_argumetns = 0
-    has_conent = True
+    has_content = True
     def run(self):
         thenode = slug(text=self.arguments[0])
         return [thenode]
@@ -17,3 +17,44 @@ class Slug(rst.Directive):
 rst.directives.register_directive('slug', Slug)
 
 
+class tags(Special, Invisible, FixedTextElement):
+    pass
+
+class Tags(rst.Directive):
+    required_arguments = 1
+    optional_argumetns = 0
+    has_content = True
+    final_argument_whitespace = True
+    def run(self):
+        thenode = tags(text=self.arguments[0])
+        return [thenode]
+
+rst.directives.register_directive('tags', Tags)
+
+class abstract(Special, Invisible, FixedTextElement):
+    pass
+
+class Abstract(rst.Directive):
+    required_arguments = 1
+    optional_argumetns = 0
+    has_content = True
+    final_argument_whitespace = True
+    def run(self):
+        thenode = abstract(text=self.arguments[0])
+        return [thenode]
+
+rst.directives.register_directive('abstract', Abstract)
+
+class taxonomy(Special, Invisible, FixedTextElement):
+    pass
+
+class Taxonomy(rst.Directive):
+    required_arguments = 1
+    optional_argumetns = 0
+    has_content = True
+    final_argument_whitespace = False
+    def run(self):
+        thenode = taxonomy(text=self.arguments[0])
+        return [thenode]
+
+rst.directives.register_directive('taxonomy', Taxonomy)
