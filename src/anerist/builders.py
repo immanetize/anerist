50b24107ae88066313d57b4db7726d4a06195078
Pete Travis <immanetize@fedoraproject.org>
2015-09-28 22:50:16 -0500

    make vcs buildstep generation more reusable

--- src/anerist/builders.py
+++ src/anerist/builders.py
@@ -7,21 +7,77 @@ from buildbot.steps.shell import ShellCommand
 #
 # I just realized this expects lists to be ordered just how I expect.  Don't do that.
 
-class docbook_factory_generator():
+    
+class vcs_factory_generator():
     def __init__(self):
         pass
     
-    def fetch(self, resource_name, vcs_type, repourl):
+    def vcs_clean(self, resource_name, vcs_type, repourl):
+        new_step = []
+        if vcstype == "git":
+            newstep.append(
+                ShellCommand(                    
+                    name = "%s git cleanup" % resource_name,
+                    command = ["/usr/bin/git", "clean", "-xdf"]
+                    )
+        return new_step
+    
+    def vcs_fetch(self, resource_name, vcs_type, repourl):
         new_step = []
         if vcstype == "git":
             newstep.append(
                 Git(
-                    name = "%s fetch" % resource_name,
+                    name = "%s git fetch" % resource_name,
                     repourl = repourl,
                     mode = 'incremental'
                     )
         return new_step
+      
+    def vcs_branch(self, vcs_type, branch_name):
+        new_step = []
+        if vcstype == "git":
+            newstep.append(
+                ShellCommand(
+                    name = "git branch %s " % branch_name,
+                    command = ["/usr/bin/git", "branch", branch_name]
+                    mode = 'incremental'
+                    )
+        return new_step
     
+    def vcs_checkout(self, vcs_type, branch_name):
+        new_step = []
+        if vcstype == "git":
+            newstep.append(
+                ShellCommand(
+                    name = "git checkout %s " % branch_name,
+                    command = ["/usr/bin/git", "checkout", branch_name]
+                    )
+        return new_step
+
+    def vcs_merge(self, vcs_type, branch_name):
+        new_step = []
+        if vcstype == "git":
+            newstep.append(
+                ShellCommand(
+                    name = "git merge %s " % branch_name,
+                    command = ["/usr/bin/git", "merge", branch_name]
+                    )
+        return new_step
+
+    def vcs_commit(self, resource_name, vcs_type, repourl, commit_msg):
+        new_step = []
+        if vcstype == "git":
+            newstep.append(
+                ShellCommand(
+                    name = "%s git commit" % resource_name,
+                    command = ["/usr/bin/git", "commit", "-m", commit_msg]
+
+        return new_step
+    
+    
+class docbook_factory_generator():
+    def __init__(self):
+        pass
     def publican_build(self, langs, formats):
         new_step = []
         new_step.append(
@@ -72,6 +128,7 @@ class docbook_factory_generator():
     def parse_everything(
         resource_name,
         vcs_type='git',
+        vcs_pattern,
         repourl,
         langs,
         formats,
