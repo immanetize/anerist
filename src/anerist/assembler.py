7295a7167da2c51e0eb7a692e3f77bcdde760fb9
Pete Travis <immanetize@fedoraproject.org>
2015-10-14 00:24:08 -0500

    validate during assembly

--- src/anerist/assembler.py
+++ src/anerist/assembler.py
@@ -1,6 +1,8 @@
 from anerist.file_handlers import file_handlers
+from anerist.meta import validator
 
 hander = file_handlers()
+validator = validator()
 
 class assemble():
     def collect_json(self, target):
@@ -8,7 +10,8 @@ class assemble():
         for json in target:
             meta = file_handlers.load_json(metadata=json)
             for data in meta:
-                collection.append(data)
+                if validator.validate(data):
+                    collection.append(data)
         return collection
             
 
