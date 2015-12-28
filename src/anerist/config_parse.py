5ce1b11ba519bd16c853dbd8b1b27d6dabf9793d
Pete Travis <immanetize@fedoraproject.org>
2015-12-28 14:54:05 -0600

    start code for creating builders from a drop-in config file

---
+++ src/anerist/config_parse.py
@@ -0,0 +1,11 @@
+import yaml
+
+class FactoryBuilder():
+    def load_yaml(self, filename)
+        y  = open(filename)
+        project = yaml.load(y)
+        y.close()
+        return project
+
+
+
